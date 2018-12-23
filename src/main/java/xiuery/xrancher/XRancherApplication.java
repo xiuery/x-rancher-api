cad99fbcb1545546c4b21ca47c335a34d614dbf8
wangzhi <674287450@qq.com>
2018-12-23 22:39:56 +0800

    init x-rancher api for rancher2.1

---
+++ src/main/java/xiuery/xrancher/XRancherApplication.java
@@ -0,0 +1,14 @@
+package xiuery.xrancher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XRancherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XRancherApplication.class, args);
+	}
+
+}
+
