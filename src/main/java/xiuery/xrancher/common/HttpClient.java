c771db650e00ffc5b9fa0826f2f4ad67815be8f8
wangzhi <674287450@qq.com>
2019-05-09 11:12:03 +0800

    Update: http client, rancher config

--- src/main/java/xiuery/xrancher/common/HttpClient.java
+++ src/main/java/xiuery/xrancher/common/HttpClient.java
@@ -1,15 +1,18 @@
 package xiuery.xrancher.common;
 
+import org.apache.http.client.utils.URIBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Map;
 
 /**
@@ -21,21 +24,30 @@ public class HttpClient {
     @Autowired
     private RestTemplate restTemplate;
 
-    private URI createURI(String url) {
+    /**
+     * 构建URL,包含参数部分
+     * 如: https://rancher.xiuery.com/v3/projects?name=xiuery
+     */
+    private URI createURI(String url, @Nullable Map<String, String> params) {
         try {
-            return  URI.create(url);
-        } catch (IllegalArgumentException e) {
+            URIBuilder uriBuilder = new URIBuilder(url);
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                uriBuilder.addParameter(entry.getKey(), entry.getValue());
+            }
+            return uriBuilder.build();
+        } catch (URISyntaxException e) {
             throw new RuntimeException("URI构建失败");
         }
     }
 
-    private RequestEntity createRequestEntity( MultiValueMap<String, String> headers, HttpMethod method, String url) {
-        return new RequestEntity<>(headers, method, createURI(url));
+    private RequestEntity createRequestEntity( MultiValueMap<String, String> headers, HttpMethod method,
+                                               String url, @Nullable Map<String, String> params) {
+        return new RequestEntity<>(headers, method, createURI(url, params));
     }
 
     public Map<String, Object> doGet(String url, MultiValueMap<String, String> headers, Map<String, String> params){
 
-        RequestEntity requestEntity = createRequestEntity(headers, HttpMethod.GET, url);
+        RequestEntity requestEntity = createRequestEntity(headers, HttpMethod.GET, url, params);
 
         ParameterizedTypeReference<Map<String, Object>> paramTypeRef = new ParameterizedTypeReference<Map<String, Object>>() {};
         ResponseEntity<Map<String, Object>> responseEntity = restTemplate.exchange(requestEntity, paramTypeRef);
@@ -45,7 +57,7 @@ public class HttpClient {
 
     public void doPost(String url, MultiValueMap<String, String> headers, Map<String, String> body) {
 
-        RequestEntity requestEntity = createRequestEntity(headers, HttpMethod.POST, url);
+        RequestEntity requestEntity = createRequestEntity(headers, HttpMethod.POST, url, null);
 
         ResponseEntity<Object> responseEntity = restTemplate.exchange(requestEntity, Object.class);
 
--- src/main/java/xiuery/xrancher/config/XRancherConfig.java
+++ src/main/java/xiuery/xrancher/config/XRancherConfig.java
@@ -7,22 +7,47 @@ import org.springframework.context.annotation.Configuration;
 @Data
 @Configuration
 @ConfigurationProperties(
-    prefix = "x-config.rancher"
+    prefix = "x-config.rancher-server"
 )
 public class XRancherConfig {
 
     /**
      * rancher 服务器地址
      */
+
     private String url;
 
     /**
-    * token
-    */
+     * token
+     */
     private String token;
 
     /**
      * header
      */
     private String contentType;
+    
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
 }
